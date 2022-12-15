6c1ea53394fd82ac0d30667381f364244390b6e1
Begislan <begislanbayastanuulu2001@gmail.com>
2022-12-15 11:55:49 +0600

    del

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,6 @@
+from django.urls import reverse
 from django.db import models
-
-# Create your models here.
 from django.db import models
-
-# Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(
@@ -14,3 +11,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("post_detail", args=[str(self.pk)])
+    
