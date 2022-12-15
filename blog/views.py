59a4dde9f2bb631412c95ce30580794f7fc07475
Begislan <begislanbayastanuulu2001@gmail.com>
2022-12-15 12:55:26 +0600

    project

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Post
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
